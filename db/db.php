f545a211ba1d98157c959ba39e37149749ba7f51
root <root@LAPTOP-0LMNPK4C.localdomain>
2020-03-25 01:03:38 +0900

    20200325_hayashi_v3

--- db/db.php
+++ db/db.php
@@ -1,7 +1,7 @@
 <?php
 function db_connect() {
     //DB名とユーザー名、パスワードは任意。
-    $dsn = 'mysql:us-cdbr-iron-east-04.cleardb.net;dbname=heroku_1fde2782f9ecf81;charset=utf8';
+    $dsn = 'mysql:us-cdbr-iron-east-04.cleardb.net;dbname=heroku_1fde2782f9ecf81;charset=utf8;unix_socket=/tmp/mysql.sock';
     $user = 'b16ffc7ca81c3c';
     $password = 'd38db1c4';
 
